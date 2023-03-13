7c3f82a6725cb74ef2a0e4a478332d7e737907ab
Anagnostakis Ioannis <412135+rizitis@users.noreply.github.com>
2023-03-13 08:02:01 +0200

    Add files via upload

---
+++ requaired.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# rizitis 03/2023 GR 
+
+dir=/tmp/slack-required
+for i in `cat gnome-all.txt`; do
+mkdir -p $dir
+find ./ -name $i.info -exec cp {} $dir \;
+cat $dir/$i.info | grep REQUIRES= > $dir/$i.txt
+sed -i 's/REQUIRES=//g' $dir/*.txt
+sed -i 's/^"//' $dir/*.txt
+sed -i 's/"$//' $dir/*.txt
+tr -cs [:alnum:] '\n' < $dir/$i.txt > $dir/$i.req
+rm $dir/*.txt $dir/*.info
+done
